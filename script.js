88bc3141d9aba383cc65f8755269885cf519e917
Miwa G <miwa.guhres@hotmail.com>
2023-09-04 22:38:28 +0200

    edit index layout

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ console.log(input_item.value.toLowerCase());
 
 function searchItem (){
     for (let i = 0; i< dropdown_items.length; i++){
-        dropdown_items[i].style.display="none";
+        
         if(dropdown_items[i].innerText.toLowerCase().includes(input_item.value.toLowerCase())){
         /*show all dropdown_items included in input_item */
+            /*document.getElementById('list').style.display="block";*/
             dropdown_items[i].style.display="block";
         }else{
             dropdown_items[i].style.display="none";
+            /*document.getElementById('list').style.display="none";*/
         }
     }
 
